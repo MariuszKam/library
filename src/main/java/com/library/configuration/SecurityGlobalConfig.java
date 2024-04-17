8fe36a0fbc957cf7bf18f2ac34210538b856fcc1
Mariusz <rexxarhell@gmail.com>
2024-04-17 19:21:33 +0200

    Move PasswordEncoder outside in case of multiple SecurityFilterChains

--- src/main/java/com/library/configuration/SecurityGlobalConfig.java
+++ src/main/java/com/library/configuration/SecurityGlobalConfig.java
@@ -5,13 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityGlobalConfig {
+
+    private final PasswordEncoderConfig passwordEncoderConfig;
+
+    public SecurityGlobalConfig(PasswordEncoderConfig passwordEncoderConfig) {
+        this.passwordEncoderConfig = passwordEncoderConfig;
+    }
+
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -22,8 +27,4 @@ public class SecurityConfig {
                 .build();
     }
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
